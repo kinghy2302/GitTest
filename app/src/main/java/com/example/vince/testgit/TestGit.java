aa6b92c55c27da8418e58abd5a1eecc5ce98d09b
Vince He <kinghy2302@sina.com>
2015-07-18 14:13:53 +0800

    commit 1

--- app/src/main/java/com/example/vince/testgit/TestGit.java
+++ app/src/main/java/com/example/vince/testgit/TestGit.java
@@ -4,4 +4,8 @@ package com.example.vince.testgit;
  * Created by vince on 2015/7/18.
  */
 public class TestGit {
+
+    public static void main(String[] args){
+        System.out.println("github");
+    }
 }
