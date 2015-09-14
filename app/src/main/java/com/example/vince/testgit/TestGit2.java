4f6ba3c39bfd6ba459760c6290fb4ca167ede667
Vince He <kinghy2302@sina.com>
2015-09-14 13:37:19 +0800

    test branch commit

---
+++ app/src/main/java/com/example/vince/testgit/TestGit2.java
@@ -0,0 +1,11 @@
+package com.example.vince.testgit;
+
+/**
+ * Created by vince on 2015/7/18.
+ */
+public class TestGit {
+
+    public static void main(String[] args){
+        System.out.println("github");
+    }
+}
